a44d471fa65e39f40b0cdb23e503eac588a05d35
julian1c2a <julian.calderon.almendros@gmail.com>
2025-06-06 12:36:55 +0200

    feat: agregar script de construcción para la biblioteca PeanoNatLib

---
+++ lake_build.bash
@@ -0,0 +1,15 @@
+echo
+lake build PeanoNatLib.PeanoNatLib
+echo
+lake build PeanoNatLib.PeanoNatAxioms
+echo
+lake build PeanoNatLib.PeanoNatStrictOrder
+echo
+lake build PeanoNatLib.PeanoNatOrder
+echo
+lake build PeanoNatLib.PeanoNatMaxMin
+echo
+lake build PeanoNatLib.PeanoNatAdd
+echo
+lake build PeanoNatLib.PeanoNatSub
+echo "Hecho"

e7b8f72f34a00c003f221b8b30bd573097a3ad3f
julian1c2a <julian.calderon.almendros@gmail.com>
2025-08-18 16:49:46 +0200

    feat: agregar script de construcción para ZFCAxiomSetTheory

---
+++ build.bash
@@ -0,0 +1,11 @@
+echo
+lake build ZFCAxiomSetTheory.Extension
+echo
+lake build ZFCAxiomSetTheory.Existence
+echo
+lake build ZFCAxiomSetTheory.Specification
+echo
+lake build ZFCAxiomSetTheory.Pairing
+echo
+lake build ZFCAxiomSetTheory.Union
+echo "Hecho"
